2f5716c7d7a63b67e22fd65420cd057b7a5d85cb
Victor Nani <victor.nani1996@gmail.com>
2025-12-16 23:51:10 +0200

    Update of UI and JS logic
    Was updated UI of the fetching data
    Update of the JS logic

--- script.js
+++ script.js
@@ -1,38 +1,120 @@
-// http://www.omdbapi.com/?apikey=a7323cd&t=${input_info}
-
 const formEl = document.querySelector(".form-container");
 const mainEl = document.querySelector("main");
 
+mainEl.addEventListener("click", (e) => {
+  if (e.target.classList[0] === "read-more") {
+    e.target.parentNode.classList.toggle("hidden");
+
+    e.target.parentNode.parentNode
+      .querySelector(".more-text")
+      .classList.toggle("hidden");
+  }
+
+  if (e.target.classList[0] === "read-less") {
+    e.target.parentNode.classList.toggle("hidden");
+
+    e.target.parentNode.parentNode
+      .querySelector(".less-text")
+      .classList.toggle("hidden");
+  }
+});
+
 formEl.addEventListener("submit", async (e) => {
   e.preventDefault();
+
   const inputValue = document.querySelector("#movie-search").value;
 
   const movieData = await getMovie(inputValue);
 
   const markUpForMovie = await generateMarkUp(movieData);
 
-  console.log(movieData);
-
   mainEl.innerHTML = markUpForMovie;
 });
 
 async function getMovie(movie) {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-    },
-  };
-
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?query=${movie}`,
-    options
+    `https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=98cf43282363c475cdae6ed924d1de80`
   );
   const data = await res.json();
-  return data;
+  return data.results;
 }
 
 async function generateMarkUp(movie) {
+  const firstFive = movie.slice(0, 5);
+
+  const movieWithGenre = getGenreDescription(firstFive);
+  const moviesWithDuration = await getDurationMovie(movieWithGenre);
+
+  const generatedMarkUp = moviesWithDuration
+    .map((movie) => {
+      const btn =
+        movie.overview.length <= 130
+          ? ""
+          : `<button class="read-more description-btn"> ...Read More</button>`;
+      return `
+      <div class="movie">
+          <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              alt="Poster of the ${movie.title}"
+              class="movie-poster"
+          />
+          <div class="movie-details">
+              <h3 class="general-title">
+              ${movie.title}
+              <span class="general-star">
+                  <i class="fa-solid fa-star" style="color: #ffd43b"></i>
+                  ${movie.vote_average.toFixed(1)}
+              </span>
+              </h3>
+              <div class="duration-and-genre">
+                  <p class="duration">${movie.duration} min</p>
+                  <p class="genre">${movie.genre}</p>
+                  <button class="add-to-watchlist">
+                      <span>
+                      <i class="fa-solid fa-circle-plus"></i>
+                      </span>
+                      Watchlist
+                  </button>
+              </div>
+              <div class="movie-details-description">
+                <p class="less-text">
+                ${movie.overview.slice(0, 130)}
+                ${btn}
+                <p class="more-text hidden">${movie.overview}
+                <button class="read-less description-btn " >Read Less</button>
+                </p>
+                <span class="more-text hidden">${movie.overview.slice(130)} 
+                
+                </span>
+                </p>
+                
+              </div>
+          </div>
+      </div>`;
+    })
+    .join("");
+
+  return generatedMarkUp;
+}
+
+async function getDurationMovie(movies) {
+  const movieWithDuration = await Promise.all(
+    movies.map(async (movie) => {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${movie.id}?api_key=98cf43282363c475cdae6ed924d1de80`
+        );
+        const data = await res.json();
+        return { ...movie, duration: data.runtime };
+      } catch (err) {
+        console.error(err);
+      }
+    })
+  );
+  return movieWithDuration;
+}
+
+function getGenreDescription(movies) {
   const allGenres = [
     { id: 28, name: "Action" },
     { id: 12, name: "Adventure" },
@@ -54,40 +136,14 @@ async function generateMarkUp(movie) {
     { id: 10752, name: "War" },
     { id: 37, name: "Western" },
   ];
-  const firstFive = await movie.results.slice(0, 5);
-  const generatedMarkUp = await firstFive
-    .map(
-      (movie) => `
-        <div class="movie">
-            <img
-                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                alt="Poster of the ${movie.title}"
-                class="movie-poster"
-            />
-            <div class="movie-details">
-                <h3 class="general-title">
-                ${movie.title}
-                <span class="general-star">
-                    <i class="fa-solid fa-star" style="color: #ffd43b"></i>
-                    ${movie.vote_average}
-                </span>
-                </h3>
 
-                <div class="duration-and-genre">
-                    <p class="duration">${movie.id}</p>
-                    <p class="genre">${movie.genre_ids.join(",")}</p>
-                    <button class="add-to-watchlist">
-                        <span>
-                        <i class="fa-solid fa-circle-plus"></i>
-                        </span>
-                        Watchlist
-                    </button>
-                </div>
-                <p class="movie-description">${movie.overview}</p>
-            </div>
-        </div>`
-    )
-    .join("");
+  const moviesWithGenres = movies.map((movie) => {
+    const movieGenre = allGenres
+      .filter((genres) => movie.genre_ids.includes(genres.id))
+      .map((objGenre) => objGenre.name)
+      .join(", ");
+    return { ...movie, genre: movieGenre };
+  });
 
-  return generatedMarkUp;
+  return moviesWithGenres;
 }
